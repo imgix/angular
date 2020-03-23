c00746e2718141091cb3943248877197d2261e72
Sherwin H <sherwin@imgix.com>
2020-03-23 14:18:05 -0700

    test: add scaffold for an example application using ng-imgix

---
+++ projects/ng-imgix-example/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { NgImgixModule } from 'ng-imgix';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgImgixModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
