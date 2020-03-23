ebcc55f759faefedf9ce8cf216612fdaa36aeecc
Sherwin H <sherwin@imgix.com>
2020-03-23 12:42:48 -0700

    feat: a directive to intelligently build srcsets     - exposes an attribute directive that can be used on `<img>` elements, given that the `src` is a valid imgix URL     - parses the `src` attribute and passes it through imgix-core-js     - based on certain dimensional parameters, will generate either a fluid or fixed size srcset     - merges any other existing parameters back into the newly-constructed `srcset

--- projects/ng-imgix/src/lib/ng-imgix.module.ts
+++ projects/ng-imgix/src/lib/ng-imgix.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
-import { NgImgixComponent } from './ng-imgix.component';
 import { IxSrcsetDirective } from './ix-srcset.directive';
 
 
 
 @NgModule({
-  declarations: [NgImgixComponent, IxSrcsetDirective],
+  declarations: [IxSrcsetDirective],
   imports: [
   ],
-  exports: [NgImgixComponent]
+  exports: [IxSrcsetDirective]
 })
 export class NgImgixModule { }
