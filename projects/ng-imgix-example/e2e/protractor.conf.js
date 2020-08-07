7044279780d1f68e0149f6aefbcaae43423e0f86
Frederick Fogerty <frederick.fogerty@gmail.com>
2020-08-07 13:24:52 +0200

    chore: change refs of master to main

--- projects/ng-imgix-example/e2e/protractor.conf.js
+++ projects/ng-imgix-example/e2e/protractor.conf.js
@@ -1,6 +1,6 @@
 // @ts-check
 // Protractor configuration file, see link for more information
-// https://github.com/angular/protractor/blob/master/lib/config.ts
+// https://github.com/angular/protractor/blob/main/lib/config.ts
 
 const { SpecReporter } = require('jasmine-spec-reporter');
 
