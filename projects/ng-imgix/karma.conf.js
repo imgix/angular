23b22e77dde07205fdd4b8f79d987dceb9b50ac1
sherwinski <sherwin@imgix.com>
2020-08-18 15:12:06 -0700

    config(karma): remove "autoWatch: true"

--- projects/ng-imgix/karma.conf.js
+++ projects/ng-imgix/karma.conf.js
@@ -25,7 +25,6 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
     failOnEmptyTestSuite: true,
     browsers: ['Chrome', 'ChromeHeadlessCI', 'FirefoxHeadless'],
     customLaunchers: {
