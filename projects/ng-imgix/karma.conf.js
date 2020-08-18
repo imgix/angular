30e23877a7bf396999fa175535385522448f0719
sherwinski <sherwin@imgix.com>
2020-08-18 14:40:03 -0700

    config(karma): add failOnEmptyTestSuite option

--- projects/ng-imgix/karma.conf.js
+++ projects/ng-imgix/karma.conf.js
@@ -26,6 +26,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
+    failOnEmptyTestSuite: true,
     browsers: ['Chrome', 'ChromeHeadlessCI', 'FirefoxHeadless'],
     customLaunchers: {
       ChromeHeadlessCI: {
