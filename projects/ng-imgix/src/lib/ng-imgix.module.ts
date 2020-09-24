d41100dd782f9e00d325101545fdcb5ba4c1d8c9
Frederick Fogerty <frederick.fogerty@gmail.com>
2020-09-24 11:55:45 +0200

    fix: module definition

--- projects/ng-imgix/src/lib/ng-imgix.module.ts
+++ projects/ng-imgix/src/lib/ng-imgix.module.ts
@@ -2,11 +2,7 @@ import { ModuleWithProviders, NgModule } from '@angular/core';
 import { ImgixConfig, ImgixConfigService } from './imgix-config.service';
 import { ImgixComponent } from './imgix.component';
 
-@NgModule({
-  declarations: [ImgixComponent],
-  imports: [],
-  exports: [ImgixComponent],
-})
+@NgModule()
 export class NgImgixModule {
   // Using config injection pattern from: https://medium.com/@michelestieven/angular-writing-configurable-modules-69e6ea23ea42
   static forRoot(config: ImgixConfig): ModuleWithProviders {
