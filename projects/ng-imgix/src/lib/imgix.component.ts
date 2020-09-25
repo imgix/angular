fd5e2cf10491c9f5e5172624f32dcfb88fdefd28
Frederick Fogerty <frederick.fogerty@gmail.com>
2020-09-25 16:04:25 +0200

    feat: more basic imgix component, and pass through html attributes

--- projects/ng-imgix/src/lib/imgix.component.ts
+++ projects/ng-imgix/src/lib/imgix.component.ts
@@ -1,16 +1,35 @@
-import { Component, Inject, Injectable, Input } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Inject,
+  Injectable,
+  Input,
+  ViewChild,
+} from '@angular/core';
 import ImgixClient from 'imgix-core-js';
 import { ImgixConfig, ImgixConfigService } from './imgix-config.service';
 
 @Injectable()
 @Component({
-  selector: 'ix-img',
-  template: `<img [src]="srcURL" />`, // TODO: add srcset
+  // the [src] means that src is required
+  selector: 'ix-img[src]',
+  template: `<img [src]="srcURL" #v />`, // TODO: add srcset
 })
 export class ImgixComponent {
-  private readonly client;
+  private readonly client: ImgixClient;
+
+  @ViewChild('v', { static: false })
+  v?: ElementRef<HTMLImageElement>;
 
   @Input() src: string;
+  @Input() fixed?: boolean;
+  @Input() imgixParams?: Object;
+  @Input() width?: string | number;
+  @Input() height?: string | number;
+  @Input() attributeConfig?: Object;
+  @Input() disableVariableQuality?: boolean;
+
+  @Input() htmlAttributes?: Object;
 
   constructor(@Inject(ImgixConfigService) config: ImgixConfig) {
     this.client = new ImgixClient({
@@ -18,7 +37,18 @@ export class ImgixComponent {
     });
   }
 
+  ngAfterViewInit() {
+    this.setHTMLAttributes();
+  }
+
+  private setHTMLAttributes() {
+    const el = this.v.nativeElement;
+    Object.entries(this.htmlAttributes ?? {}).map(([key, value]) =>
+      el.setAttribute(key, value),
+    );
+  }
+
   get srcURL(): string {
-    return this.client.buildURL(this.src);
+    return this.client.buildURL(this.src, this.imgixParams);
   }
 }
