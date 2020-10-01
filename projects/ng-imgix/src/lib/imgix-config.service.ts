72f83a76770b900bc0151f29d63894c2bcf0e85e
Frederick Fogerty <frederick.fogerty@gmail.com>
2020-10-01 16:10:51 +0200

    feat: default params

--- projects/ng-imgix/src/lib/imgix-config.service.ts
+++ projects/ng-imgix/src/lib/imgix-config.service.ts
@@ -1,8 +1,10 @@
 import { InjectionToken } from '@angular/core';
+import { IImgixParams } from './types';
 
 export interface ImgixConfig {
   domain: string;
   includeLibraryParam?: boolean;
+  defaultImgixParams?: IImgixParams;
 }
 
 export const ImgixConfigService = new InjectionToken<ImgixConfig>(
