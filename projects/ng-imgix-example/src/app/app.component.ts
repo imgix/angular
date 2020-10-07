efd2b395f0157396b0e9f8774ebb92316323076b
Frederick Fogerty <frederick.fogerty@gmail.com>
2020-10-07 16:17:07 +0200

    chore: test lazy loading directive in example app

--- projects/ng-imgix-example/src/app/app.component.ts
+++ projects/ng-imgix-example/src/app/app.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   // templateUrl: './app.component.html',
-  template: ` <ix-img src="unsplash/motorbike.jpg"></ix-img> `,
+  template: `<ix-img
+    src="unsplash/motorbike.jpg"
+    [attributeConfig]="{ src: 'data-src', srcset: 'data-srcset' }"
+    lazy-img
+    loading="lazy"
+  ></ix-img>`,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
