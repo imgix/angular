2f997be2b94a01a4da6fb34a51809897ca2305f3
Frederick Fogerty <frederick.fogerty@gmail.com>
2020-10-07 12:45:42 +0200

    chore: remove ixSrcset directive

--- projects/ng-imgix-example/src/app/app.component.ts
+++ projects/ng-imgix-example/src/app/app.component.ts
@@ -3,17 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   // templateUrl: './app.component.html',
-  template: `
-    <img
-      src="https://assets.imgix.net/unsplash/motorbike.jpg"
-      [ixSrcset]="{ minWidth: 800, maxWidth: 1200 }"
-    />
-    <img src="https://assets.imgix.net/unsplash/bridge.jpg?w=500" ixSrcset />
-    <img
-      src="https://assets.imgix.net/unsplash/raspberries.jpg?w=900"
-      [ixSrcset]="{ disableVariableQuality: true }"
-    />
-  `,
+  template: ` <ix-img src="unsplash/motorbike.jpg"></ix-img> `,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
