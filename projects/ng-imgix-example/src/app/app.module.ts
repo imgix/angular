8c7bc58ad7ad4b340e7687158d19075c9ae11160
Frederick Fogerty <frederick.fogerty@gmail.com>
2020-10-07 12:58:26 +0200

    config: setup ng-imgix correctly in test app

--- projects/ng-imgix-example/src/app/app.module.ts
+++ projects/ng-imgix-example/src/app/app.module.ts
@@ -1,12 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
 import { NgImgixModule } from 'ng-imgix';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, NgImgixModule],
+  imports: [
+    BrowserModule,
+    NgImgixModule.forRoot({
+      domain: 'assets.imgix.net',
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
