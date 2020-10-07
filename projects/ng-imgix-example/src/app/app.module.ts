50449f45b4e32b997c1536ef24118bddfe153fe6
Frederick Fogerty <frederick.fogerty@gmail.com>
2020-10-07 16:17:07 +0200

    chore: test lazy loading directive in example app

--- projects/ng-imgix-example/src/app/app.module.ts
+++ projects/ng-imgix-example/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgImgixModule } from 'ng-imgix';
 import { AppComponent } from './app.component';
+import { LazyloadDirective } from './lazyload.directive';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LazyloadDirective],
   imports: [
     BrowserModule,
     NgImgixModule.forRoot({
       domain: 'assets.imgix.net',
+      defaultImgixParams: {
+        auto: 'format,compress',
+      },
     }),
   ],
   providers: [],
