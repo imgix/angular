ed4b5e47b247ee0ab96e061e6c067acfd53774c6
Frederick Fogerty <frederick.fogerty@gmail.com>
2020-10-13 17:24:02 +0200

    feat: ix-picture component

--- projects/imgix-ng/src/lib/imgix-ng.module.ts
+++ projects/imgix-ng/src/lib/imgix-ng.module.ts
@@ -1,11 +1,12 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { ImgixConfig, ImgixConfigService } from './imgix-config.service';
 import { ImgixComponent } from './imgix.component';
+import { IxPictureComponent } from './ix-picture.component';
 import { IxSourceComponent } from './ix-source.component';
 
 @NgModule({
-  declarations: [ImgixComponent, IxSourceComponent],
-  exports: [ImgixComponent, IxSourceComponent],
+  declarations: [ImgixComponent, IxSourceComponent, IxPictureComponent],
+  exports: [ImgixComponent, IxSourceComponent, IxPictureComponent],
 })
 export class NgImgixModule {
   // Using config injection pattern from: https://medium.com/@michelestieven/angular-writing-configurable-modules-69e6ea23ea42
@@ -15,6 +16,7 @@ export class NgImgixModule {
       providers: [
         ImgixComponent,
         IxSourceComponent,
+        IxPictureComponent,
         { provide: ImgixConfigService, useValue: config },
       ],
     };
