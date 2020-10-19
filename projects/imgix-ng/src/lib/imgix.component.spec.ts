3503e320ba94861c6c8cb93d2d5f13e1cd162e15
Frederick Fogerty <frederick.fogerty@gmail.com>
2020-10-19 14:38:26 +0200

    test: update tests after attribute selector refactor

--- projects/imgix-ng/src/lib/imgix.component.spec.ts
+++ projects/imgix-ng/src/lib/imgix.component.spec.ts
@@ -2,14 +2,14 @@ import {
   expectElementToHaveFixedSrcAndSrcSet,
   expectElementToHaveFluidSrcAndSrcSet,
   expectEverySrcsetToMatch,
-  expectURLToHaveIxlibParam
+  expectURLToHaveIxlibParam,
 } from '../test/url-assert';
 import { renderTemplate } from '../test/util';
 
 describe('Imgix Component', () => {
   it('should combine the domain and path into the src', async () => {
     expect(
-      (await renderImgTemplate(`<img ix-img path='amsterdam.jpg' />`))
+      (await renderImgTemplate(`<img ixImg path='amsterdam.jpg' />`))
         .getComponent()
         .getAttribute('src'),
     ).toMatch('https://assets.imgix.net/amsterdam.jpg');
@@ -17,7 +17,7 @@ describe('Imgix Component', () => {
 
   it('should set srcset correctly', async () => {
     const srcset = (
-      await renderImgTemplate(`<img ix-img path='amsterdam.jpg' />`)
+      await renderImgTemplate(`<img ixImg path='amsterdam.jpg' />`)
     )
       .getComponent()
       .getAttribute('srcset');
@@ -38,7 +38,7 @@ describe('Imgix Component', () => {
     expect(
       (
         await renderImgTemplate(
-          `<img ix-img path='amsterdam.jpg' [imgixParams]="{txt: 'Hello'}" />`,
+          `<img ixImg path='amsterdam.jpg' [imgixParams]="{txt: 'Hello'}" />`,
         )
       )
         .getComponent()
@@ -50,7 +50,7 @@ describe('Imgix Component', () => {
     expectEverySrcsetToMatch('txt=Hello')(
       (
         await renderImgTemplate(
-          `<img ix-img path='amsterdam.jpg' [imgixParams]="{txt: 'Hello'}" />`,
+          `<img ixImg path='amsterdam.jpg' [imgixParams]="{txt: 'Hello'}" />`,
         )
       )
         .getComponent()
@@ -60,7 +60,7 @@ describe('Imgix Component', () => {
 
   it('should pass through html attributes', async () => {
     const test = await renderImgTemplate(
-      `<img ix-img path="amsterdam.jpg" class="img-class"  />`,
+      `<img ixImg path="amsterdam.jpg" class="img-class"  />`,
     );
 
     expect(test.getComponent().getAttribute('class')).toBe('img-class');
@@ -83,7 +83,7 @@ describe('Imgix Component', () => {
     TEST_CASES.map(([html, attribute, expectedValue]) => {
       it(`${attribute} should be ${expectedValue} when the html attribute is ${html}`, async () => {
         const test = await renderImgTemplate(
-          `<img ix-img path="amsterdam.jpg" ${html} />`,
+          `<img ixImg path="amsterdam.jpg" ${html} />`,
         );
 
         expect(test.getComponent().getAttribute(attribute)).toBe(expectedValue);
@@ -92,25 +92,25 @@ describe('Imgix Component', () => {
   });
 
   describe('in fluid mode (no fixed props set)', () => {
-    it('ix-img should render a fluid image if width is passed as attribute with no fixed attribute', async () => {
+    it('ixImg should render a fluid image if width is passed as attribute with no fixed attribute', async () => {
       const test = await renderImgTemplate(
-        `<img ix-img path="amsterdam.jpg" width="100"  />`,
+        `<img ixImg path="amsterdam.jpg" width="100"  />`,
       );
 
       expectElementToHaveFluidSrcAndSrcSet(test.getComponent());
       expect(test.getComponent().getAttribute('width')).toBe('100');
     });
-    it('ix-img should render a fluid image if width is passed as attribute with fixed="false" attribute', async () => {
+    it('ixImg should render a fluid image if width is passed as attribute with fixed="false" attribute', async () => {
       const test = await renderImgTemplate(
-        `<img ix-img path="amsterdam.jpg" width="100" fixed="false" />`,
+        `<img ixImg path="amsterdam.jpg" width="100" fixed="false" />`,
       );
 
       expectElementToHaveFluidSrcAndSrcSet(test.getComponent());
       expect(test.getComponent().getAttribute('width')).toBe('100');
     });
-    it('ix-img should render a fluid image if width is passed as attribute with [fixed]="false" attribute', async () => {
+    it('ixImg should render a fluid image if width is passed as attribute with [fixed]="false" attribute', async () => {
       const test = await renderImgTemplate(
-        `<img ix-img path="amsterdam.jpg" width="100" [fixed]="false" />`,
+        `<img ixImg path="amsterdam.jpg" width="100" [fixed]="false" />`,
       );
 
       expectElementToHaveFluidSrcAndSrcSet(test.getComponent());
@@ -118,25 +118,25 @@ describe('Imgix Component', () => {
     });
     it('a width attribute should be passed through to the underlying component', async () => {
       const test = await renderImgTemplate(
-        `<img ix-img path="amsterdam.jpg" width="100" />`,
+        `<img ixImg path="amsterdam.jpg" width="100" />`,
       );
       expect(test.getComponent().getAttribute('width')).toBe('100');
     });
     it('a width attribute should not be set on the underlying component if width is not passed', async () => {
       const test = await renderImgTemplate(
-        `<img ix-img path="amsterdam.jpg" />`,
+        `<img ixImg path="amsterdam.jpg" />`,
       );
       expect(test.getComponent().hasAttribute('width')).toBe(false);
     });
     it('a height attribute should be passed through to the underlying component', async () => {
       const test = await renderImgTemplate(
-        `<img ix-img path="amsterdam.jpg" height="100" />`,
+        `<img ixImg path="amsterdam.jpg" height="100" />`,
       );
       expect(test.getComponent().getAttribute('height')).toBe('100');
     });
     it('a height attribute should not be set on the underlying component if height is not passed', async () => {
       const test = await renderImgTemplate(
-        `<img ix-img path="amsterdam.jpg" />`,
+        `<img ixImg path="amsterdam.jpg" />`,
       );
       expect(test.getComponent().hasAttribute('height')).toBe(false);
     });
@@ -145,14 +145,14 @@ describe('Imgix Component', () => {
   describe('in fixed mode (fixed prop set, or width/height passed to imgixParams)', () => {
     it('the src and srcset should be in fixed size mode when a width is passed to imgixParams', async () => {
       const test = await renderImgTemplate(
-        `<img ix-img path="amsterdam.jpg" [imgixParams]="{w: 100}"  />`,
+        `<img ixImg path="amsterdam.jpg" [imgixParams]="{w: 100}"  />`,
       );
 
       expectElementToHaveFixedSrcAndSrcSet(test.getComponent(), 100);
     });
     it('the src and srcset should be in fixed size mode when a fixed prop is passed to the element', async () => {
       const test = await renderImgTemplate(
-        `<img ix-img path="amsterdam.jpg" width="100" height="150" fixed />`,
+        `<img ixImg path="amsterdam.jpg" width="100" height="150" fixed />`,
       );
 
       const el = test.getComponent();
@@ -169,7 +169,7 @@ describe('Imgix Component', () => {
     });
     it('the src and srcset should be in fixed size mode when a fixed="true" prop is passed to the element', async () => {
       const test = await renderImgTemplate(
-        `<img ix-img path="amsterdam.jpg" width="100" height="150" fixed="true" />`,
+        `<img ixImg path="amsterdam.jpg" width="100" height="150" fixed="true" />`,
       );
 
       const el = test.getComponent();
@@ -182,7 +182,7 @@ describe('Imgix Component', () => {
     });
     it('the src and srcset should be in fixed size mode when a [fixed]="true" prop is passed to the element', async () => {
       const test = await renderImgTemplate(
-        `<img ix-img path="amsterdam.jpg" width="100" height="150" [fixed]="true" />`,
+        `<img ixImg path="amsterdam.jpg" width="100" height="150" [fixed]="true" />`,
       );
 
       const el = test.getComponent();
@@ -196,13 +196,13 @@ describe('Imgix Component', () => {
 
     it('a width attribute should be passed through to the underlying component', async () => {
       const test = await renderImgTemplate(
-        `<img ix-img path="amsterdam.jpg" width="100" fixed />`,
+        `<img ixImg path="amsterdam.jpg" width="100" fixed />`,
       );
       expect(test.getComponent().getAttribute('width')).toBe('100');
     });
     it('a height attribute should be passed through to the underlying component', async () => {
       const test = await renderImgTemplate(
-        `<img ix-img path="amsterdam.jpg" height="100" fixed />`,
+        `<img ixImg path="amsterdam.jpg" height="100" fixed />`,
       );
       expect(test.getComponent().getAttribute('height')).toBe('100');
     });
@@ -213,7 +213,7 @@ describe('Imgix Component', () => {
     ATTRIBUTES.forEach((attribute) => {
       it(`${attribute} can be configured to use data-${attribute}`, async () => {
         const test = await renderImgTemplate(
-          `<img ix-img path="amsterdam.jpg" [attributeConfig]="{'${attribute}': 'data-${attribute}'}"  />`,
+          `<img ixImg path="amsterdam.jpg" [attributeConfig]="{'${attribute}': 'data-${attribute}'}"  />`,
         );
 
         expect(test.getComponent().getAttribute(`data-${attribute}`)).toMatch(
@@ -227,7 +227,7 @@ describe('Imgix Component', () => {
   describe('disableVariableQuality', () => {
     it('should not disable variable quality by default', async () => {
       const test = await renderImgTemplate(
-        `<img ix-img path="amsterdam.jpg" fixed width="100" height="100" />`,
+        `<img ixImg path="amsterdam.jpg" fixed width="100" height="100" />`,
       );
 
       const srcset = test.getComponent().getAttribute('srcset');
@@ -242,7 +242,7 @@ describe('Imgix Component', () => {
     });
     it('the srcset should have a variable quality when disableVariableQuality attribute set', async () => {
       const test = await renderImgTemplate(
-        `<img ix-img path="amsterdam.jpg" fixed width="100" height="100" disableVariableQuality />`,
+        `<img ixImg path="amsterdam.jpg" fixed width="100" height="100" disableVariableQuality />`,
       );
 
       const srcset = test.getComponent().getAttribute('srcset');
@@ -254,7 +254,7 @@ describe('Imgix Component', () => {
   describe('ixlib param', () => {
     it(`should be present by default and equal to "ng"`, async () => {
       const test = await renderImgTemplate(
-        `<img ix-img path="amsterdam.jpg"  />`,
+        `<img ixImg path="amsterdam.jpg"  />`,
       );
 
       const expectIxlibParam = expectURLToHaveIxlibParam('ng');
@@ -266,7 +266,7 @@ describe('Imgix Component', () => {
 
     it(`should contain the correct package version`, async () => {
       const test = await renderImgTemplate(
-        `<img ix-img path="amsterdam.jpg"  />`,
+        `<img ixImg path="amsterdam.jpg"  />`,
       );
 
       const packageVersion = require('../../package.json').version;
@@ -281,7 +281,7 @@ describe('Imgix Component', () => {
 
     it(`should not be included in src/srcset when includeLibraryParam is false`, async () => {
       const test = await renderImgTemplate(
-        `<img ix-img path="amsterdam.jpg"  />`,
+        `<img ixImg path="amsterdam.jpg"  />`,
         { domain: 'assets.imgix.net', includeLibraryParam: false },
       );
 
@@ -294,7 +294,7 @@ describe('Imgix Component', () => {
   describe('default parameters', () => {
     it('should be applied to src', async () => {
       const test = await renderImgTemplate(
-        `<img ix-img path="amsterdam.jpg" />`,
+        `<img ixImg path="amsterdam.jpg" />`,
         {
           domain: 'assets.imgix.net',
           defaultImgixParams: {
@@ -310,7 +310,7 @@ describe('Imgix Component', () => {
     });
     it('should be applied to srcset', async () => {
       const test = await renderImgTemplate(
-        `<img ix-img path="amsterdam.jpg" />`,
+        `<img ixImg path="amsterdam.jpg" />`,
         {
           domain: 'assets.imgix.net',
           defaultImgixParams: {
@@ -326,7 +326,7 @@ describe('Imgix Component', () => {
     });
     it('should be overridden by locally set parameters', async () => {
       const test = await renderImgTemplate(
-        `<img ix-img path="amsterdam.jpg" [imgixParams]="{txt: 'Overridden'}" />`,
+        `<img ixImg path="amsterdam.jpg" [imgixParams]="{txt: 'Overridden'}" />`,
         {
           domain: 'assets.imgix.net',
           defaultImgixParams: {
